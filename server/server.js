3ded63af0e35a242532e173ee967bbd8175ccc16
DISHA23S <sdisha3574@gmail.com>
2024-03-29 19:15:39 +0530

    jty

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 // Middleware
 app.use(cors());
